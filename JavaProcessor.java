604975f1418daeeecb60abb8ac1315f62cd522d3
Gordie White <gordiewhite@Gordies-MacBook-Pro.local>
2017-04-24 16:29:46 -0500

    fixed formatting errors

--- JavaProcessor.java
+++ JavaProcessor.java
@@ -1,37 +1,37 @@
 import java.io.*;
 
 public class JavaProcessor {
-	public static void main(String[] args) {
-		File file = new File(args[0]);
-		BufferedReader reader = null;
-		PrintWriter writer = null;
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        BufferedReader reader = null;
+        PrintWriter writer = null;
 
-		try {
-			writer = new PrintWriter("Monitor.java", "UTF-8");
-    		reader = new BufferedReader(new FileReader(file));
-    		String text = null;
+        try {
+            writer = new PrintWriter("Monitor.java", "UTF-8");
+            reader = new BufferedReader(new FileReader(file));
+            String text = null;
 
-    		while ((text = reader.readLine()) != null) {
-    			//need to cycle through until monitor class
-    			//then add in additional stuff into the function
-    			//to satisfy monitor functionality
-    			System.out.println(text);
-        		writer.println(text);
-    		}
-		} catch (FileNotFoundException e) {
-    		e.printStackTrace();
-		} catch (IOException e) {
-    		e.printStackTrace();
-		} finally {
-    		try {
-        		if (reader != null) {
-            		reader.close();
-        		}
-        		if (writer != null) {
-        			writer.close();
-        		}
-    		} catch (IOException e) {
-    		}
-		}
-	}
+            while ((text = reader.readLine()) != null) {
+//need to cycle through until monitor class
+//then add in additional stuff into the function
+//to satisfy monitor functionality
+                System.out.println(text);
+                writer.println(text);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
 }
