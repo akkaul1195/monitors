b0560ee63dc483e9568c2fc11654cf132715d74a
Gordie White <gordiewhite@wireless-10-146-123-33.public.utexas.edu>
2017-04-24 19:52:56 -0500

    initial commit with full code *not tested*

--- JavaProcessor.java
+++ JavaProcessor.java
@@ -10,13 +10,92 @@ public class JavaProcessor {
             writer = new PrintWriter("Monitor.java", "UTF-8");
             reader = new BufferedReader(new FileReader(file));
             String text = null;
+            boolean inMonitor = false;
+            int bracketCounter = 0;
 
             while ((text = reader.readLine()) != null) {
-//need to cycle through until monitor class
-//then add in additional stuff into the function
-//to satisfy monitor functionality
-                System.out.println(text);
-                writer.println(text);
+                //need to cycle through until monitor class
+                if (text.contains("monitor class")) {
+                    text = text.replace("monitor ", "");
+                    inMonitor = true;
+                    writer.println(text);
+                    writer.println("private final Lock mutex = new ReentrantLock();");
+                    writer.println("private final Condition isEmpty = mutex.newCondition();");
+                    bracketCounter += 1;
+                    text = reader.readLine();
+                }
+                if (inMonitor == true && text.contains("{")) {
+                    bracketCounter += 1;
+                }
+                if (inMonitor == true && text.contains("}")) {
+                    bracketCounter -= 1;
+                    if (inMonitor && bracketCounter == 0) {
+                        inMonitor = false;
+                    }
+                }
+                //then add in additional stuff into the function
+                //to satisfy monitor functionality
+
+                if (inMonitor) {
+                    if (text.contains(" await(")) {
+                        writer.println(text.replace("{", "") + "throws InterruptedException {");
+                        writer.println("mutex.lock();");
+                        writer.println("try {");
+                        int bCount = 1;
+                        while((text = reader.readLine()) != null) {
+                            if (text.contains("{")) {
+                                bCount += 1;
+                            }
+                            if (text.contains("}")) {
+                                bCount -= 1;
+                            }
+                            if (bCount == 0) {
+                                writer.println("} finally {");
+                                writer.println("mutex.unlock();");
+                                writer.println("}");
+                                writer.println(text);
+                                break;
+                            } else if (text.contains("waituntil(")) {
+                                String cond = text.replace("waituntil", "");
+                                cond = cond.replace(";", "");
+                                writer.println("while (!" + cond + ") {");
+                                writer.println("isEmpty.await()");
+                                writer.println("}");
+                            } else {
+                                writer.println(text);
+                            }
+                        }
+                    } else if (text.contains(" unlock(")) {
+                        writer.println(text.replace("{", "") + "throws InterruptedException {");
+                        writer.println("mutex.lock();");
+                        writer.println("try {");
+                        int bCount = 1;
+                        while((text = reader.readLine()) != null) {
+                            if (text.contains("{")) {
+                                bCount += 1;
+                            }
+                            if (text.contains("}")) {
+                                bCount -= 1;
+                            }
+                            if (bCount == 0) {
+                                writer.println("} finally {");
+                                writer.println("mutex.unlock();");
+                                writer.println("}");
+                                writer.println(text);
+                                break;
+                            } else if (text.contains("return ")) {
+                                writer.println("isEmpty.signalAll();");
+                                writer.println(text);
+                            } else {
+                                writer.println(text);
+                            }
+                        }
+                    } else {
+                        writer.println(text);
+                    }
+                } else {
+                    writer.println(text);
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
